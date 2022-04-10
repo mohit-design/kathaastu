0fbbbff1a1a36bdf238de2af380fec5ed7ac3160
Mohit Patel <45488208+mohit-design@users.noreply.github.com>
2022-04-10 10:38:23 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function() {
-  
+  // Mobile Burger Menu JS
+  $(".burger-menu").click(function() {
+    $(this).toggleClass("active");
+    $("body").toggleClass("active");
+    $(".bg-blur").toggleClass("active");
+    $(".fixed-sidebar").toggleClass("active");
+  });
 });
